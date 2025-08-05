9056c2b632d51c3c6604a1e5e4658abebae7a0ce
abhinav-pratapsingh <abhinavpratap6142@gmail.com>
2025-08-05 20:08:58 +0530

    resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,18 @@
 import pic from "../assets/photo.png"
+import resume from "../assets/Abhinav_Resume.pdf"
 import "../components/Hero.css"
 
 const Hero = ()=>{
+    const handleClick = ()=>{
+        window.open(resume,'_blank');
+    }
     return(
         <div className="hero__section">
             <div className="hero__container">
             <div className="hero__info">
                 <h1>Hello, I'm Abhinav Pratap</h1>
                 <p>Aspiring Fullstack Web Developer</p>
-                <button>Download Resume</button>
+                <button onClick={handleClick}>Download Resume</button>
             </div>
             <div className="hero__pic">
                 <img src={pic} alt="developer image"/>
